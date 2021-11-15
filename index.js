ecdcd8bf6e2eb2b2d8055e89c3db0169ce369ea0
GartnerF <gartner.florian@gmx.net>
2021-11-15 11:47:03 +0100

    updatewebsite

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!w3");
+    response.end("Hallo von der anderen seite");
 });
 
 const port = process.env.PORT || 1337;
